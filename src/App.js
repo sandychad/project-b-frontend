2c30c345a6958af52598219d667845157bfdb079
sandychad <sandychad@outlook.com>
2020-06-17 19:04:29 -0500

    installed react and set up hello world

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// React
+import React, { Component } from 'react';
+
+// React Bootstrap
+import { Container } from 'react-bootstrap';
+
+// Local Styles
+import './css/App.css';
+
+export class App extends Component {
+  render() {
+    return (
+      <Container>
+        <h1>Hello World</h1>
+      </Container>
+    );
+  }
+}
+
+export default App;
