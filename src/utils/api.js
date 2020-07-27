719874ac1fadce485707e32ef8e551e526b6b38f
sandychad <sandychad@outlook.com>
2020-07-27 18:15:37 -0500

    changed backend url to env variable

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,17 @@
 // Import Axios to make HTTP Requests
 import axios from 'axios';
 
+const backendPath = process.env.BACKEND_PATH
+  ? process.env.BACKEND_PATH
+  : 'http://localhost:8000/';
+
 // Create api object that will be used to call APIs in Redux Actions
 const api = axios.create({
   // Heroku
   // baseURL: 'https://dry-castle-10158.herokuapp.com/',
 
   // Development
-  baseURL: 'http://localhost:8000/',
+  baseURL: backendPath,
 
   // Add header for content-type for POST requests
   headers: {
