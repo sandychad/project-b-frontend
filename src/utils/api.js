21929b086dfa8aafacb13b3c9c79b09376d6b14f
sandychad <sandychad@outlook.com>
2020-07-27 18:41:11 -0500

    update api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,17 +1,13 @@
 // Import Axios to make HTTP Requests
 import axios from 'axios';
 
-const backendPath = process.env.BACKEND_PATH
-  ? process.env.BACKEND_PATH
-  : 'http://localhost:8000/';
-
 // Create api object that will be used to call APIs in Redux Actions
 const api = axios.create({
   // Heroku
   // baseURL: 'https://dry-castle-10158.herokuapp.com/',
 
   // Development
-  baseURL: backendPath,
+  baseURL: process.env.BACKEND_PATH,
 
   // Add header for content-type for POST requests
   headers: {
