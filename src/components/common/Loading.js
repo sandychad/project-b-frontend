24bb087142b5af5e1e73f891533315d24cf86b49
sandychad <sandychad@outlook.com>
2020-07-27 20:59:19 -0500

    Loading as function

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -1,19 +1,17 @@
 // Import React
-import React, { Component } from 'react';
+import React from 'react';
 
 // React Bootstrap
 import { Container, Spinner } from 'react-bootstrap';
 
-export class Loading extends Component {
-  render() {
-    return (
-      <Container className='text-center'>
-        <Spinner variant='primary' animation='border' role='status'>
-          <span className='sr-only'>Loading...</span>
-        </Spinner>
-      </Container>
-    );
-  }
-}
+export const Loading = () => {
+  return (
+    <Container className='text-center'>
+      <Spinner variant='primary' animation='border' role='status'>
+        <span className='sr-only'>Loading...</span>
+      </Spinner>
+    </Container>
+  );
+};
 
 export default Loading;
