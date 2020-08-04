5ce30163373d77c37e85b1fafd0b2ac8542a5c21
Sudhachad <sudhachad@live.com>
2020-08-04 10:01:39 -0700

    Updated auto width using ResponsiveContainer Property

--- src/components/Reports/AverageTemperature.js
+++ src/components/Reports/AverageTemperature.js
@@ -6,6 +6,7 @@ import {
   YAxis,
   ZAxis,
   CartesianGrid,
+  ResponsiveContainer,
   Tooltip,
   Legend,
 } from 'recharts';
@@ -17,52 +18,52 @@ export default class AverageTemperature extends PureComponent {
     const { data } = this.props;
 
     return (
-      <ScatterChart
-        width={1000}
-        height={600}
-        margin={{
-          top: 20,
-          right: 30,
-          left: 20,
-          bottom: 65,
-        }}
-      >
-        <CartesianGrid />
-        <XAxis
-          dataKey='location'
-          name='Location'
-          angle='-60'
-          tickMargin='40'
-          position='left'
-          padding={{ left: 50, right: 50 }}
-        />
-        <YAxis
-          type='number'
-          dataKey='avgTemperature'
-          name='Average Temperature'
-          domain={[94, 106]}
-          unit='°F'
-          label={{
-            value: 'Average Temperature',
-            angle: -90,
-            position: 'insideLeft',
+      <ResponsiveContainer width={'100%'} height={600}>
+        <ScatterChart
+          margin={{
+            top: 20,
+            right: 30,
+            left: 20,
+            bottom: 65,
           }}
-        />
-        <ZAxis
-          type='number'
-          dataKey='totalpeople'
-          range={[100, 1000]}
-          name='Total People'
-        />
-        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-        <Legend verticalAlign='top' height={50} />
-        <Scatter
-          name='Average Temperature'
-          data={data}
-          fill='#8884d8'
-          shape='star'
-        />
-      </ScatterChart>
+        >
+          <CartesianGrid />
+          <XAxis
+            dataKey='location'
+            name='Location'
+            angle='-60'
+            tickMargin='40'
+            position='left'
+            padding={{ left: 50, right: 50 }}
+          />
+          <YAxis
+            type='number'
+            dataKey='avgTemperature'
+            name='Average Temperature'
+            domain={[94, 106]}
+            unit='°F'
+            label={{
+              value: 'Average Temperature',
+              angle: -90,
+              position: 'insideLeft',
+            }}
+          />
+          <ZAxis
+            type='number'
+            dataKey='totalpeople'
+            range={[100, 1000]}
+            name='Total People'
+          />
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+          <Legend verticalAlign='top' height={50} />
+          <Scatter
+            name='Average Temperature'
+            data={data}
+            fill='#8884d8'
+            shape='star'
+          />
+        </ScatterChart>
+      </ResponsiveContainer>
     );
   }
 }
--- src/components/Reports/EmployeeTempResults.js
+++ src/components/Reports/EmployeeTempResults.js
@@ -5,66 +5,80 @@ import {
   XAxis,
   YAxis,
   CartesianGrid,
+  ResponsiveContainer,
   Tooltip,
   Legend,
   ReferenceLine,
+  Cell,
 } from 'recharts';
+import { scaleOrdinal } from 'd3-scale';
+import { schemeCategory10 } from 'd3-scale-chromatic';
+
+const colors = scaleOrdinal(schemeCategory10).range();
 
 export default class Example extends PureComponent {
-  static jsfiddleUrl = 'https://jsfiddle.net/alidingling/uLysj0u2/';
+  static jsfiddleUrl = 'https://jsfiddle.net/alidingling/9Lfxjjty/';
 
   render() {
     const { data } = this.props;
-
     return (
-      <ScatterChart
-        width={800}
-        height={500}
-        data={data}
-        margin={{
-          top: 20,
-          right: 30,
-          left: 20,
-          bottom: 65,
-        }}
-      >
-        <CartesianGrid />
-        <XAxis
-          dataKey='Employee ID'
-          angle='-60'
-          tickMargin='40'
-          position='left'
-          padding={{ left: 50, right: 50 }}
-        />
-        <YAxis
-          dataKey='Temperature'
-          domain={[90, 110]}
-          unit='°F'
-          label={{
-            value: 'Temperature',
-            angle: -90,
-            position: 'insideLeft',
-          }}
-        />
-        <Tooltip cursor={{ strokeDasharray: '1 1' }} />
-        <Legend verticalAlign='top' height={50} />
-        <ReferenceLine
-          y={104}
-          label={{ position: 'top', value: 'Upper Limit' }}
-          stroke='red'
-        />
-        <ReferenceLine
-          y={96}
-          label={{ position: 'bottom', value: 'Lower Limit' }}
-          stroke='green'
-        />
-        <Scatter
+      <ResponsiveContainer width={'100%'} height={600}>
+        <ScatterChart
+          width={1000}
+          height={500}
           data={data}
-          fill='#8884d8'
-          type='monotone'
-          dataKey='Location'
-        />
-      </ScatterChart>
+          margin={{
+            top: 20,
+            right: 30,
+            left: 20,
+            bottom: 65,
+          }}
+        >
+          <CartesianGrid />
+          <XAxis
+            dataKey='Employee ID'
+            angle='-60'
+            tickMargin='40'
+            position='left'
+            padding={{ left: 50, right: 50 }}
+          />
+          <YAxis
+            dataKey='Temperature'
+            domain={[93, 106]}
+            unit='°F'
+            label={{
+              value: 'Temperature',
+              angle: -90,
+              position: 'insideLeft',
+            }}
+          />
+          <Legend verticalAlign='top' height={50} layout='vertical' />
+          <Tooltip cursor={{ strokeDasharray: '1 1' }} />
+          <ReferenceLine
+            y={104}
+            label={{ position: 'top', value: 'Upper Limit' }}
+            stroke='red'
+          />
+          <ReferenceLine
+            y={96}
+            label={{ position: 'bottom', value: 'Lower Limit' }}
+            stroke='green'
+          />
+          <Scatter
+            //dataKey='Location'
+            data={data}
+            fill='#8884d8'
+            name='Locatoion by color'
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={colors[index % colors.length]}
+              />
+            ))}
+          </Scatter>
+        </ScatterChart>
+      </ResponsiveContainer>
     );
   }
 }
