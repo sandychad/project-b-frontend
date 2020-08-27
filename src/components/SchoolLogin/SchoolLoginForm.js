02c38e3b263ef21719ef9b6f0a4e52f681b4e29f
sandychad <sandychad@outlook.com>
2020-08-26 20:04:07 -0500

    form UI

--- src/components/SchoolLogin/SchoolLoginForm.js
+++ src/components/SchoolLogin/SchoolLoginForm.js
@@ -8,15 +8,17 @@ export default function SchoolLogin(props) {
   return (
     <Container fluid>
       <Form noValidate>
-        <Form.Group as={Col} md={{ span: 4, offset: 4 }}>
-          <Row>
-            <Form.Label>Login Code</Form.Label>
-            <Form.Label>{props.user_hash}</Form.Label>
-          </Row>
-          <Row>
-            <Form.Label>Student ID</Form.Label>
-            <Form.Text className='text-muted'>Student ID</Form.Text>
-          </Row>
+        <Form.Group as={Row} md={{ span: 4, offset: 4 }}>
+          <Form.Label>Login Code</Form.Label>
+          <Col md={6}>
+            <Form.Control plaintext readOnly defaultValue={props.user_hash} />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} md={{ span: 4, offset: 4 }}>
+          <Form.Label>Student ID</Form.Label>
+          <Col md={6}>
+            <Form.Control type='text' className='text-muted' />
+          </Col>
         </Form.Group>
         <Form.Group as={Col} md={{ span: 2, offset: 7 }}>
           <Button variant='primary' type='submit'>
